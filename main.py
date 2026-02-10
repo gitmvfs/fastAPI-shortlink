1449b62c7b0bda7f3ccc916551bd1affd0c400ab
gitmvfs <marcossantos8002@gmail.com>
2026-02-10 14:28:44 -0300

    Feat(default_server): config CORS acess and callback endpoint

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+
+app = FastAPI()
+
+#set CORS config for deploy
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"], 
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get("/")
+def callback_check():
+    return {"Status": "Online", "Version": "1.0.0","Current_Time": datetime.today()}

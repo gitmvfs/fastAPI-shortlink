5821065d8e3ef0f69c8cf4f8cbe229ad95f624b2
gitmvfs <marcossantos8002@gmail.com>
2026-02-19 17:18:52 -0300

    feat(ScyllaDB): connection database

---
+++ database/connection.py
@@ -0,0 +1,36 @@
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import create_keyspace_simple
+from cassandra.cluster import Cluster
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+#env
+node_database_string = str(os.getenv('node_database'))
+node_database_list = node_database_string.split(',')
+replication_factor = int(os.getenv('replication_factor'))
+
+#Allow permissions schema managment
+os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
+
+#Innitial connection 
+cluster = Cluster(node_database_list)
+session = cluster.connect()
+
+#Create keyspace for local dev
+keyspace_name = 'database_url'
+
+def get_database_session():
+    try:
+        connection.setup(node_database_list, keyspace_name, protocol_version=3)
+        
+        create_keyspace_simple(keyspace_name, replication_factor=replication_factor)
+        
+        session = cluster.connect(keyspace_name)
+        
+        print(f'ScyllaDB connected to successfully!')
+        return session
+    except Exception as e:
+        print(f'Error in database process: {e}')
+        return None

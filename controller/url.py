072aeb695149c6bce1508420d83ac9f58df9a858
gitmvfs <marcossantos8002@gmail.com>
2026-02-19 17:04:16 -0300

    feat(url): strip protocol
    Remove prefixe https:// from original url before storage in database

--- controller/url.py
+++ controller/url.py
@@ -6,15 +6,25 @@ from database.schema.link import Link
 db_session = get_database_session()
 link = Link(db_session)
 
-def new_url(original_url):
+def new_url(url):
     
     try:
         id = generate_id()
         hash = link_shortener(id)
-        link.insert_link(hash, original_url)
+        link.insert_link(hash, url)
         
         return hash
     
     except Exception as e:
         print('Error to create new URL:', e)
 
+def strip_protocol(original_url:str):
+    
+    try:
+        url = original_url.split('https://')[1]
+        print(url)
+        
+        return url
+    
+    except Exception as e:
+        print('Error to remove protocol from URL:', e)

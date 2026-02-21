9df65c4203e9267868783f22907b0f0f1d9abcbc
gitmvfs <marcossantos8002@gmail.com>
2026-02-21 18:26:15 -0300

    refactor(link): create prepare_statements query and get_link function
    switch the arch to prepare_statements increase the capacity of database to work with a lot volume of data, the queries doent's created at call function moment improving the process

--- database/schema/link.py
+++ database/schema/link.py
@@ -1,10 +1,12 @@
 from cassandra.cluster import Session
+import datetime
 
 class Link():
     
-    def __init__(self,session:Session):
+    def __init__(self, session:Session):
         self.session = session 
         self.create_table()
+        self._prepare_statements()
         
     def create_table(self):
         query = """
@@ -15,7 +17,16 @@ class Link():
             PRIMARY KEY(id)
             )"""
         self.session.execute(query)
+    
+    def _prepare_statements(self):
+        self.post_link_query = self.session.prepare("INSERT INTO short_link (id,link,created_at) VALUES (?, ?, ?) USING TTL 315360000")
+        self.get_link_query = self.session.prepare("SELECT link FROM short_link WHERE id = ?")
+    
+    def post_link(self,id_hash:str,url:str):
+        datetime_now = datetime.datetime.now(datetime.timezone.utc)
+        self.session.execute(self.post_link_query, (id_hash, url, datetime_now))
         
-    def insert_link(self,id_hash:str,url:str):
-        query = "INSERT INTO short_link (id,link,created_at) VALUES (%s, %s, toTimestamp(now()))"
-        self.session.execute(query, (id_hash, url))
+    def get_link(self, hash):
+        data = self.session.execute(self.get_link_query,[hash])
+        data = data.one()
+        return data['link']

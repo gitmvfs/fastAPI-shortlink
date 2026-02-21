b350a708435f6fb306fa5eacad783adc7e5f4bcf
gitmvfs <marcossantos8002@gmail.com>
2026-02-21 18:15:11 -0300

    feat(route): redirect endpoint

--- routes/routes.py
+++ routes/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Form
 from fastapi.responses import JSONResponse
-from controller.url import *
+from fastapi.responses import RedirectResponse
+import controller.url as url
 from dotenv import load_dotenv, find_dotenv
 import os
 load_dotenv(find_dotenv())
@@ -9,19 +10,26 @@ domain = os.getenv('domain_host')
 port = os.getenv('domain_port')
 domain_string = f'http://{domain}:{port}/' #Switch for Https if have SSL
 
-router = APIRouter(prefix='/shortlink',tags=['shortlink'])
+router = APIRouter()
 
 @router.post("/",status_code= 201)
 def short_link(original_url:str=Form(...,description="Url to shortner")):
     try:
-        normalize_url = strip_protocol(original_url)
-        hash = new_url(normalize_url)
-        return JSONResponse({'Message':'Success','data':{'original_url':original_url, 'short_url':f'{domain_string}/{hash}'}},status_code=201)
+        normalize_url = url.strip_protocol(original_url)
+        hash = url.new_url(normalize_url)
+        return JSONResponse({'message':'success','data':{'original_url':original_url, 'short_url':f'{domain_string}{hash}'}},status_code=201)
         
     except Exception as e:
         print(f'ERROR LOG: {e}')
-        return JSONResponse({'message': 'Internal Server Error, contact an admin or check console log'}, status_code=500)
     
-
+@router.get("/{hash}",status_code = 302)
+def redirect(hash:str):
+    try:
+       link = url.get_original_link(hash)
+       return RedirectResponse(link, status_code=302)
+    except Exception as e:
+        print(f'Error to get all links: {e}')
+        return JSONResponse({'message': 'Internal Server Error, contact an admin or check console log'}, status_code=500)
+        
     
 url_router = router

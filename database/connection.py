4e858225d2ea60b73abfe88eee7f3a6fe1b6c096
gitmvfs <marcossantos8002@gmail.com>
2026-02-21 18:13:41 -0300

    feat(connection): row_factory
    set dict to default get response on database

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,7 @@
 from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import create_keyspace_simple
 from cassandra.cluster import Cluster
+from cassandra.query import dict_factory
 from dotenv import load_dotenv, find_dotenv
 import os
 
@@ -28,6 +29,7 @@ def get_database_session():
         create_keyspace_simple(keyspace_name, replication_factor=replication_factor)
         
         session = cluster.connect(keyspace_name)
+        session.row_factory = dict_factory
         
         print(f'ScyllaDB connected to successfully!')
         return session

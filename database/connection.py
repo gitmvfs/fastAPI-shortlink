2b115aa35b688b236f1ef440e862fe8f4ed87456
gitmvfs <marcossantos8002@gmail.com>
2026-02-27 12:36:48 -0300

    refactor(ScyllaDB): force application start with database
    when we used ´try and except´, we allowed the application to initialize without the database.

--- database/connection.py
+++ database/connection.py
@@ -22,17 +22,10 @@ session = cluster.connect()
 #Create keyspace for local dev
 keyspace_name = 'database_url'
 
-def get_database_session():
-    try:
-        connection.setup(node_database_list, keyspace_name, protocol_version=3)
-        
-        create_keyspace_simple(keyspace_name, replication_factor=replication_factor)
-        
-        session = cluster.connect(keyspace_name)
-        session.row_factory = dict_factory
-        
-        print(f'ScyllaDB connected to successfully!')
-        return session
-    except Exception as e:
-        print(f'Error in database process: {e}')
-        return None
+connection.setup(node_database_list, keyspace_name, protocol_version=3)
+create_keyspace_simple(keyspace_name, replication_factor=replication_factor)
+
+session = cluster.connect(keyspace_name)
+session.row_factory = dict_factory #config dict to default responses
+
+print(f'ScyllaDB connected to successfully!')

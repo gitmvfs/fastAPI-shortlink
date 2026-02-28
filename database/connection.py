18312549960f8329e2aee594a6018dd968ae4daa
gitmvfs <marcossantos8002@gmail.com>
2026-02-28 16:29:54 -0300

    feat(ScyllaDB): add consistency level to default config

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,7 @@
 from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import create_keyspace_simple
 from cassandra.cluster import Cluster
+from cassandra import ConsistencyLevel
 from cassandra.query import dict_factory
 from dotenv import load_dotenv, find_dotenv
 import os
@@ -26,6 +27,7 @@ connection.setup(node_database_list, keyspace_name, protocol_version=3)
 create_keyspace_simple(keyspace_name, replication_factor=replication_factor)
 
 session = cluster.connect(keyspace_name)
+session.default_consistency_level = ConsistencyLevel.ONE
 session.row_factory = dict_factory #config dict to default responses
 
 print(f'ScyllaDB connected to successfully!')

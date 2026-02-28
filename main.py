9664439502dd4e289ae8990430a4ca3d851381a1
gitmvfs <marcossantos8002@gmail.com>
2026-02-28 16:27:23 -0300

    refactor(default_server): duplicated database call removed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
-from database.schema.link import Link
-from database.connection import get_database_session
 from routes.routes import url_router
 
 app = FastAPI()
@@ -16,10 +14,6 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
-
-#Config database
-db_session = get_database_session()
-Link(db_session)
 
 app.include_router(url_router)
 
